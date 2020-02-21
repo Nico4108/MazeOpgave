9e6ee92e8a10b3f31c52cc000e5dfbefdfff9b0b
nicspiegelhauer <Nicolaj.spiegel@gmail.com>
2020-02-21 15:40:43 +0100

    First draft

---
+++ src/com/company/Maze.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.LinkedList;
+
+
+public class Maze {
+    public int [][] maze;
+    public LinkedList<Position> path = new LinkedList<Position>();
+    public Position start;
+
+}
